0bce6d8921c8f89a39c57ff7922eb89520642a79
TibiD96 <danitibi18@gmail.com>
2022-11-28 17:31:21 +0200

    Test 7 Done

--- Json.Start/Json.Start/Json.Validator/JsonString.cs
+++ Json.Start/Json.Start/Json.Validator/JsonString.cs
@@ -11,6 +11,21 @@ namespace Json
 
         static bool IsDoubleQuoted(string input)
         {
+            int controlIndex;
+            const string backSlash = "\n";
+            if (input.Contains(backSlash))
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (char.IsControl(input, i))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
             return input.StartsWith('"') && input.EndsWith('"');
         }
     }
