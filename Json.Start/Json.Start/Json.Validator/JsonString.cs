ab45becfc218a71aa654cabc07b07403dbb5a23e
TibiD96 <danitibi18@gmail.com>
2022-11-29 23:25:28 +0200

    TEMA 1 DONE TEST Passed

--- Json.Start/Json.Start/Json.Validator/JsonString.cs
+++ Json.Start/Json.Start/Json.Validator/JsonString.cs
@@ -14,19 +14,19 @@ namespace Json
 
         static bool IsDoubleQuoted(string input)
         {
-            int indexing = input.Length - 1;
+            int index = input.Length - 1;
             const char controlCharacters = '\n';
             if (input.Contains('\\') || input.Contains(controlCharacters))
             {
                 for (int i = 0; i < input.Length; i++)
                 {
-                    if (char.IsControl(input, i) || input[i] == 'x' || input[indexing - 1] == '\\')
+                    if (char.IsControl(input, i) || input[i] == 'x' || input[index - 1] == '\\')
                     {
                         return false;
                     }
                 }
 
-                return input[indexing - 1] != 'u' && !char.IsDigit(input[indexing - 1]);
+                return input[index - 1] != 'u' && !char.IsDigit(input[index - 1]);
             }
 
             return input.StartsWith('"') && input.EndsWith('"');
