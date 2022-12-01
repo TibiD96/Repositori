3d64a66b45d2794ecf63d3184e40dcfe48fff684
Tiberiu Daniel Fazakas <dani.tibi18@gmail.com>
2022-12-01 23:58:19 +0200

    Numbers first 12 tests DONE

--- Json.Start/Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Start/Json.Validator/JsonNumber.cs
@@ -6,7 +6,35 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            throw new NotImplementedException();
+            return !string.IsNullOrEmpty(input) && IsNumber(input);
+        }
+
+        public static bool IsNumber(string input)
+        {
+            if (input.Contains('.'))
+            {
+                return StringContainDot(input);
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!char.IsDigit(input, i) && input[i] != '-')
+                {
+                    return false;
+                }
+            }
+
+            return input.Length <= 1 || !input.StartsWith('0');
+        }
+
+        public static bool StringContainDot(string input)
+        {
+            if (input.EndsWith('.'))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
