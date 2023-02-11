96418cae2d0dd7cfaeda44ade879ab83c9cd64c8
Tiberiu Daniel Fazakas <dani.tibi18@gmail.com>
2023-02-11 22:38:26 +0200

    Class for soccer team

---
+++ Soccer Classament OOP/SoccerTeams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SoccerTeams
+{
+    public class SoccerTeams
+    {
+        private readonly string name;
+
+        public SoccerTeams(string name)
+        {
+            this.name = name;
+        }
+    }
+}
