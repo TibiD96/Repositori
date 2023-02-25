37d9d84a6747ae23f30b1e304896c88bdfd0bfcc
Tiberiu Daniel Fazakas <dani.tibi18@gmail.com>
2023-02-26 00:01:59 +0200

    Text class, code and unitest for null or empty input have been added.

---
+++ JsonValidation/Text.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace JsonValidation
+{
+    public class Text : IPattern
+    {
+        private readonly string prefix;
+
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new Match(false, text);
+            }
+
+            return new Match(false, text);
+        }
+    }
+}
---
+++ JsonValidation/TextTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace JsonValidation
+{
+    public class TextTests
+    {
+        [Fact]
+
+        public void ReturnFalsForNullOrEmpty()
+        {
+            var boolTrue = new Text("true");
+            Assert.False(boolTrue.Match(null).Success());
+            Assert.Equal(null, boolTrue.Match(null).RemainingText());
+            Assert.False(boolTrue.Match("").Success());
+            Assert.Equal("", boolTrue.Match("").RemainingText());
+            var boolFalse = new Text("false");
+            Assert.False(boolFalse.Match(null).Success());
+            Assert.Equal(null, boolFalse.Match(null).RemainingText());
+            Assert.False(boolTrue.Match("").Success());
+            Assert.Equal("", boolTrue.Match("").RemainingText());
+        }
+    }
+}
