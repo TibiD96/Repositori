c655227d855e782075821337e8a688405b9e90e1
Tiberiu Daniel Fazakas <dani.tibi18@gmail.com>
2023-02-25 18:29:11 +0200

    Any class started unitest and code for null or empty case done.

---
+++ JsonValidation/Any.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace JsonValidation
+{
+    public class Any : IPattern
+    {
+        private readonly string accepted;
+
+        public Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new Match(false, text);
+            }
+
+            return new Match(false, text);
+
+        }
+    }
+}
---
+++ JsonValidation/AnyTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace JsonValidation
+{
+    public class AnyTests
+    {
+        [Fact]
+        public void ReturnFasleForNullOrEmpty()
+        {
+            var eNull = new Any("eE");
+            Assert.False(eNull.Match(null).Success());
+            Assert.Equal(null, eNull.Match(null).RemainingText());
+            var eEmpty = new Any("eE");
+            Assert.False(eEmpty.Match("").Success());
+            Assert.Equal("", eEmpty.Match("").RemainingText());
+        }
+    }
+}
