e3ac9e2dfb4171d5ff966fbeee865c7cc9fc92fa
TibiD96 <danitibi18@gmail.com>
2023-02-26 22:52:51 +0200

    Refractored.

--- JsonValidation/AnyTests.cs
+++ JsonValidation/AnyTests.cs
@@ -25,11 +25,14 @@ namespace JsonValidation
             var e = new Any("eE");
             Assert.True(e.Match("ea").Success());
             Assert.Equal("a", e.Match("ea").RemainingText());
+
             Assert.True(e.Match("Ea").Success());
             Assert.Equal("a", e.Match("Ea").RemainingText());
+
             var sign = new Any("-+");
             Assert.True(sign.Match("+3").Success());
             Assert.Equal("3", sign.Match("+3").RemainingText());
+
             Assert.True(sign.Match("-2").Success());
             Assert.Equal("2", sign.Match("-2").RemainingText());
         }
@@ -40,11 +43,14 @@ namespace JsonValidation
             var e = new Any("eE");
             Assert.False(e.Match("a").Success());
             Assert.Equal("a", e.Match("a").RemainingText());
+
             Assert.False(e.Match("a").Success());
             Assert.Equal("a", e.Match("a").RemainingText());
+
             var sign = new Any("-+");
             Assert.False(sign.Match("2").Success());
             Assert.Equal("2", sign.Match("2").RemainingText());
+
             Assert.False(sign.Match("2").Success());
             Assert.Equal("2", sign.Match("2").RemainingText());
         }
