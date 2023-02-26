e0e467a18fd8123ba7f26c63e01a1c29756d9ee7
TibiD96 <danitibi18@gmail.com>
2023-02-26 22:41:40 +0200

    Optional class has been added with code and unitest for the cases when a letter is given as input.

---
+++ JsonValidation/Optional.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JsonValidation
+{
+    internal class Optional : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text).Success() ? new Match(true, text[1..]) : new Match(true, text);
+        }
+    }
+}
---
+++ JsonValidation/OptionalTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace JsonValidation
+{
+    public class OptionalTests
+    {
+        [Fact]
+
+        public void ReturnTrueAndIfInputStartsWithGivenCharItGetsConsumed()
+        {
+            var a = new Optional(new Character('a'));
+            Assert.True(a.Match("abc").Success());
+            Assert.Equal("bc", a.Match("abc").RemainingText());
+
+            Assert.True(a.Match("aabc").Success());
+            Assert.Equal("abc", a.Match("aabc").RemainingText());
+
+            Assert.True(a.Match("bc").Success());
+            Assert.Equal("bc", a.Match("bc").RemainingText());
+
+            Assert.True(a.Match("").Success());
+            Assert.Equal("", a.Match("").RemainingText());
+
+            Assert.True(a.Match(null).Success());
+            Assert.Null(a.Match(null).RemainingText());
+        }
+    }
+}
