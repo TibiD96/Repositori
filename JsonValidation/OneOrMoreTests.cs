4ba3e8de4105d7df6cdd057e293b779b71fa6177
TibiD96 <danitibi18@gmail.com>
2023-03-01 19:57:47 +0200

    Unitests for OneOrMore have been finished.

--- JsonValidation/OneOrMoreTests.cs
+++ JsonValidation/OneOrMoreTests.cs
@@ -18,7 +18,7 @@ namespace JsonValidation
 
         [Fact]
 
-        public void ReturnTrueIfInputStartingCharIsInsideRangeAndGetConsumed()
+        public void ReturnTrueIfAllCharsFromStringAreInsideRange()
         {
             var a = new OneOrMore(new Range('0', '9'));
             Assert.True(a.Match("123").Success());
@@ -26,5 +26,34 @@ namespace JsonValidation
 
         }
 
+        [Fact]
+
+        public void ReturnTrueIfJustSomeCharsFromStringAreInsideRangeAndStringStartsWithIt()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.True(a.Match("1a").Success());
+            Assert.Equal("a", a.Match("1a").RemainingText());
+
+        }
+
+        [Fact]
+
+        public void ReturnFalseIfStringDontContainCharsFromRange()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.False(a.Match("bc").Success());
+            Assert.Equal("bc", a.Match("bc").RemainingText());
+
+        }
+
+        [Fact]
+
+        public void ReturnFalseIfStringContainCharsFromRangeButDontStartWithIt()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.False(a.Match("b1").Success());
+            Assert.Equal("b1", a.Match("b1").RemainingText());
+
+        }
     }
 }
