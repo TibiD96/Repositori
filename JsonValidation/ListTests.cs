0484577b49d098d00e80ba94c20e3808a816f662
TibiD96 <danitibi18@gmail.com>
2023-03-01 22:05:53 +0200

    Unitest for case when input is null has been added in class List.

--- JsonValidation/ListTests.cs
+++ JsonValidation/ListTests.cs
@@ -11,6 +11,9 @@ namespace JsonValidation
             var a = new List(new Range('0', '9'), new Character(','));
             Assert.True(a.Match("").Success());
             Assert.Equal("", a.Match("").RemainingText());
+
+            Assert.True(a.Match(null).Success());
+            Assert.Null(a.Match(null).RemainingText());
         }
     }
 }
