f88b803275b184bf8429feb40cb5513042ffb706
TibiD96 <danitibi18@gmail.com>
2023-03-01 21:44:45 +0200

    Code and unitest for case when string is null for class List have been added.

---
+++ JsonValidation/List.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JsonValidation
+{
+    public class List : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public List(IPattern element, IPattern separator)
+        {
+            this.pattern = new Sequence(new Many(element), new Many(separator));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ JsonValidation/ListTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace JsonValidation
+{
+    public class ListTests
+    {
+        [Fact]
+
+        public void ReturnTrueWhenNullOrEmpty()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            Assert.True(a.Match("").Success());
+            Assert.Equal("", a.Match("").RemainingText());
+        }
+    }
+}
