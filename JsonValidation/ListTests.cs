62b1c31820dc10f4f20428159ad78056df68ada5
TibiD96 <danitibi18@gmail.com>
2023-03-02 22:32:41 +0200

    Unitest for case when string don't contain element from list has been added.

--- JsonValidation/ListTests.cs
+++ JsonValidation/ListTests.cs
@@ -18,7 +18,7 @@ namespace JsonValidation
 
         [Fact]
 
-        public void ReturnTrueAndConsumeTheCharsIfElementSeparatorElementSeparatorElementIsAccomplished()
+        public void ReturnTrueAndConsumeTheEntireStringIfItContainsJustTheList()
         {
             var a = new List(new Range('0', '9'), new Character(','));
             Assert.True(a.Match("1,2,3").Success());
@@ -26,6 +26,27 @@ namespace JsonValidation
 
             Assert.True(a.Match("1,2,3,").Success());
             Assert.Equal(",", a.Match("1,2,3,").RemainingText());
+        }
+
+        [Fact]
+
+        public void ReturnTrueAndConsumeJustAPartOfStringIfItContainsMoreThanJustTheList()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            Assert.True(a.Match("1,2,3,").Success());
+            Assert.Equal(",", a.Match("1,2,3,").RemainingText());
+
+            Assert.True(a.Match("1a").Success());
+            Assert.Equal("a", a.Match("1a").RemainingText());
+        }
+
+        [Fact]
+
+        public void ReturnTrueEvenIfTheStringDontContainAnyOfTheListElement()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            Assert.True(a.Match("abc").Success());
+            Assert.Equal("abc", a.Match("abc").RemainingText());
         }
     }
 }
