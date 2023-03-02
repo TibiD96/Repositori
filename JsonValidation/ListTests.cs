044abfc6cf482ea875ea6eb8cac80281adb18ffb
TibiD96 <danitibi18@gmail.com>
2023-03-02 22:08:42 +0200

    Unitest for the case when <element><separator><element><separator><element> is not accomplished has been added.

--- JsonValidation/ListTests.cs
+++ JsonValidation/ListTests.cs
@@ -18,11 +18,14 @@ namespace JsonValidation
 
         [Fact]
 
-        public void ReturnTrueAndConsumeTheCharsWhichAreEqualWithPattern()
+        public void ReturnTrueAndConsumeTheCharsIfElementSeparatorElementSeparatorElementIsAccomplished()
         {
             var a = new List(new Range('0', '9'), new Character(','));
             Assert.True(a.Match("1,2,3").Success());
             Assert.Equal("", a.Match("1,2,3").RemainingText());
+
+            Assert.True(a.Match("1,2,3,").Success());
+            Assert.Equal(",", a.Match("1,2,3,").RemainingText());
         }
     }
 }
