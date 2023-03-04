ebc794a0035f5c3a656096318258df04ccb553b9
Tiberiu Daniel Fazakas <dani.tibi18@gmail.com>
2023-03-04 18:54:41 +0200

    Unitest for Null case has been added.

--- JsonValidation/NumberTests.cs
+++ JsonValidation/NumberTests.cs
@@ -11,6 +11,9 @@ namespace JsonValidation
             var number = new Number();
             Assert.False(number.Match("").Success());
             Assert.Equal("", number.Match("").RemainingText());
+
+            Assert.False(number.Match(null).Success());
+            Assert.Null(number.Match(null).RemainingText());
         }
     }
 }
