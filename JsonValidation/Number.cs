a21a4bcf4db4e34a68cb2f0400d196ce37e28859
Tiberiu Daniel Fazakas <dani.tibi18@gmail.com>
2023-03-04 18:52:18 +0200

    Number class and unitest for the Empty case has been added.

---
+++ JsonValidation/Number.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace JsonValidation
+{
+    public class Number : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Number()
+        {
+
+            var zeroToNine = new Range('0', '9');
+            this.pattern = new Sequence(zeroToNine);
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ JsonValidation/NumberTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace JsonValidation
+{
+    public class NumberTests
+    {
+        [Fact]
+
+        public void ReturnFalseForNullOrEmpty()
+        {
+            var number = new Number();
+            Assert.False(number.Match("").Success());
+            Assert.Equal("", number.Match("").RemainingText());
+        }
+    }
+}
