7f5e096266460c21cef1d016d2636d7321fb61fe
TibiD96 <danitibi18@gmail.com>
2023-03-11 21:17:21 +0200

    Cases fort escaped caracters has been added.

--- JsonValidation/StringTests.cs
+++ JsonValidation/StringTests.cs
@@ -23,7 +23,26 @@ namespace JsonValidation
             var stringWithEscapedQuotationMark = new String();
             Assert.Equal("", stringWithEscapedQuotationMark.Match(Quoted(@"\""a\"" b")).RemainingText());
 
+            var stringWithEscapedReverseSolidus = new String();
+            Assert.Equal("", stringWithEscapedReverseSolidus.Match(Quoted(@"a \\ b")).RemainingText());
 
+            var stringWithEscapedSolidus = new String();
+            Assert.Equal("", stringWithEscapedSolidus.Match(Quoted(@"a \/ b")).RemainingText());
+
+            var stringWithEscapedBackSpace = new String();
+            Assert.Equal("", stringWithEscapedBackSpace.Match(Quoted(@"a \b b")).RemainingText());
+
+            var stringWithEscapedFormFeed = new String();
+            Assert.Equal("", stringWithEscapedFormFeed.Match(Quoted(@"a \f b")).RemainingText());
+
+            var stringWithEscapedLineFeed = new String();
+            Assert.Equal("", stringWithEscapedLineFeed.Match(Quoted(@"a \n b")).RemainingText());
+
+            var stringWithEscapedCarigeReturn = new String();
+            Assert.Equal("", stringWithEscapedCarigeReturn.Match(Quoted(@"a \r b")).RemainingText());
+
+            var stringWithEscapedHorizontalTab = new String();
+            Assert.Equal("", stringWithEscapedHorizontalTab.Match(Quoted(@"a \t b")).RemainingText());
         }
 
         public static string Quoted(string text)
