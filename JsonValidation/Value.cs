5fadceb321af07c4fe5293d6ccd7df0a9b3c4604
Tiberiu Daniel Fazakas <dani.tibi18@gmail.com>
2023-03-14 14:19:02 +0200

    Value class has been refractored.

--- JsonValidation/Value.cs
+++ JsonValidation/Value.cs
@@ -12,7 +12,8 @@ namespace JsonValidation
             var number = new Number();
             var value = new Choice(stringInput, number, new Text("true"), new Text("false"), new Text("null"));
             var ws = new Many(new Any(" \r\n\t"));
-            var squareBrackets = new Many(new Any("[]"));
+            var openSquareBracket = new Character('[');
+            var closeSquareBracket = new Character(']');
             var brace = new Many(new Any("{}"));
             var element = new Sequence(ws, value, ws);
             var elements = new List(element, new Character(','));
@@ -20,7 +21,7 @@ namespace JsonValidation
             var members = new List(member, new Character(','));
 
             var obj = new Sequence(brace, members, brace);
-            var array = new Sequence(squareBrackets, elements, squareBrackets);
+            var array = new Sequence(openSquareBracket, elements, closeSquareBracket);
 
             value.Add(obj);
             value.Add(array);
