6af81fc6f93d9bcd146e89caa1edc0bc1e61a47f
Tiberiu Daniel Fazakas <dani.tibi18@gmail.com>
2023-03-14 11:56:37 +0200

    var WS has been added to Value class.

--- JsonValidation/Value.cs
+++ JsonValidation/Value.cs
@@ -8,8 +8,14 @@ namespace JsonValidation
 
         public Value()
         {
+            var stringinput = new String();
+            var number = new Number();
+            var value = new Choice(stringinput, number, new Text("true"), new Text("false"), new Text("null"));
+            var ws = new Many(new Any(" \r\n\t"));
 
-            pattern = ;
+            var array = ;
+            var obj = ;
+            pattern = value;
         }
 
         public IMatch Match(string text)
