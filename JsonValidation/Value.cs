9a900d21ff10ebd63ae804178c2cea8ed2d06b18
Tiberiu Daniel Fazakas <dani.tibi18@gmail.com>
2023-03-14 11:23:52 +0200

    Value class and value unitests have been added.

---
+++ JsonValidation/Value.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace JsonValidation
+{
+    public class Value : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Value()
+        {
+
+            pattern = ;
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
