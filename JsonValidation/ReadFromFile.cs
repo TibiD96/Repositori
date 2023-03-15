a3dd94dbd1c544e0405aceb71f00a3fbd27f61b3
Tiberiu Daniel Fazakas <dani.tibi18@gmail.com>
2023-03-15 19:40:01 +0200

    Read from file has been refractored.

--- JsonValidation/ReadFromFile.cs
+++ JsonValidation/ReadFromFile.cs
@@ -4,10 +4,18 @@ namespace JsonValidation
 {
     class ReadFromFile
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            string text = System.IO.File.ReadAllText(@"");
-
+            string text = System.IO.File.ReadAllText(@"E:\Modulul 2\JsonValidation\JSONText.txt");
+            var value = new Value(); 
+            if (value.Match(text).RemainingText() == "")
+            {
+                Console.WriteLine("Text from file is Json Valid");
+            }
+            else
+            {
+                Console.WriteLine("Text from file is not Json Valid");
+            }
         }
     }
 }
