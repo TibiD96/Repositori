44b30a7484e07b1a9dcc09aaa0d9e1f1921a5552
TibiD96 <danitibi18@gmail.com>
2023-03-21 18:18:36 +0200

    Ecuation class and EcuationTests have been created.

---
+++ JsonValidation/Ecuation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace JsonValidation
+{
+    public class Ecuation : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Ecuation()
+        {
+            var number = new Number();
+            var value = new Choice(number);
+            var ws = new Many(new Character(' '));
+            var sign = new Many(new Any("+-*/"));
+            var openBrackets = new Character('(');
+            var closeBrackets = new Character(')');
+            var element = new Sequence(value, ws);
+            var elements = new List(element, sign);
+
+            var array = new Sequence(ws, elements, ws);
+
+            value.Add(array);
+            pattern = value;
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ JsonValidation/EcuationTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace JsonValidation
+{
+    public class EcuationTests
+    {
+        [Fact]
+
+        public void ReturnTrueAndEmptyForACorrectSimpleEcuation()
+        {
+            var value = new Value();
+            Assert.True(value.Match("1 + 1").Success());
+            Assert.Equal("", value.Match("1 + 1").RemainingText());
+        }
+    }
+}
