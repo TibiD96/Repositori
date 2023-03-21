855fe753f9e14f1e18e48dbcde60d9e8d43653e7
TibiD96 <danitibi18@gmail.com>
2023-03-21 21:39:47 +0200

    case for single number has been added.

--- JsonValidation/EcuationTests.cs
+++ JsonValidation/EcuationTests.cs
@@ -12,5 +12,14 @@ namespace JsonValidation
             Assert.True(value.Match("1 + 1").Success());
             Assert.Equal("", value.Match("1 + 1").RemainingText());
         }
+
+        [Fact]
+
+        public void ReturnTrueAndEmptyForAOperationWithOneNumber()
+        {
+            var value = new Ecuation();
+            Assert.True(value.Match("1").Success());
+            Assert.Equal("", value.Match("1 + 1").RemainingText());
+        }
     }
 }
