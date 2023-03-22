4de7d7d95b5640cc3f37f6cb58e9ec5b58375884
TibiD96 <danitibi18@gmail.com>
2023-03-23 00:22:40 +0200

    Ecuation tests has been refractored.

--- JsonValidation/EcuationTests.cs
+++ JsonValidation/EcuationTests.cs
@@ -18,7 +18,7 @@ namespace JsonValidation
         public void ReturnTrueAndEmptyForAOperationWithOneNumber()
         {
             var value = new Ecuation();
-            Assert.True(value.Match(" 1 ").Success());
+            Assert.True(value.Match("1").Success());
             Assert.Equal("", value.Match("1").RemainingText());
         }
 
@@ -33,11 +33,17 @@ namespace JsonValidation
 
         [Fact]
 
-        public void ReturnFalseAndReturnRemainingTextUncorrectEcuationNoBrackets()
+        public void ReturnRemainingTextForInvalidEcuationNoBrackets()
         {
             var value = new Ecuation();
             Assert.True(value.Match("1 + 1 +").Success());
             Assert.Equal(" +", value.Match("1 + 1 +").RemainingText());
+
+            Assert.True(value.Match("1 + 1 + - 1").Success());
+            Assert.Equal(" + - 1", value.Match("1 + 1 + - 1").RemainingText());
+
+            Assert.True(value.Match("- 1 + 1").Success());
+            Assert.Equal("- 1 + 1", value.Match("- 1 + 1").RemainingText());
         }
 
         [Fact]
@@ -51,11 +57,78 @@ namespace JsonValidation
 
         [Fact]
 
-        public void ReturnTrueAndEmptyForEcuationWithOaneSetsOfBracketsAndComplexEcuation()
+        public void ReturnTrueAndEmptyForEcuationWithOaneLevelBrackets()
         {
             var value = new Ecuation();
             Assert.True(value.Match("( 1 + 1 + 1 )").Success());
             Assert.Equal("", value.Match("( 1 + 1 + 1 )").RemainingText());
+        }
+
+        [Fact]
+
+        public void ReturnTrueAndEmptyForEcuationWithMoreLevelsBrackets()
+        {
+            var value = new Ecuation();
+            Assert.True(value.Match("( 1 + 1 + 1 ) + ( 5 - 2 + 3 * 1 )").Success());
+            Assert.Equal("", value.Match("( 1 + 1 + 1 ) + ( 5 - 2 + 3 * 1 )").RemainingText());
+
+            Assert.True(value.Match("( 1 + 1 + 1 ) + ( ( 5 - 2 ) + ( 3 * 1 ) )").Success());
+            Assert.Equal("", value.Match("( 1 + 1 + 1 ) + ( ( 5 - 2 ) + ( 3 * 1 ) )").RemainingText());
+        }
+
+        [Fact]
+
+        public void ReturnTrueAndEmptyForComplexEcuationWithBrackets()
+        {
+            var value = new Ecuation();
+            Assert.True(value.Match("( 1 + 1 + 1 ) + 5").Success());
+            Assert.Equal("", value.Match("( 1 + 1 + 1 ) + 5").RemainingText());
+
+            Assert.True(value.Match("( 1 + 1 + 1 ) + ( ( 5 - 2 ) * 10 )").Success());
+            Assert.Equal("", value.Match("( 1 + 1 + 1 ) + ( ( 5 - 2 ) * 10 )").RemainingText());
+
+            Assert.True(value.Match("( ( 5 + 10 ) )").Success());
+            Assert.Equal("( ( 5 + 10 ) )", value.Match("( ( 5 + 10 ) )").RemainingText());
+        }
+
+        [Fact]
+
+        public void ReturnRemainingTextForInvalidEcuationWithBrackets()
+        {
+            var value = new Ecuation();
+            Assert.True(value.Match("( 1 + 1 + 1 ) - ( 10 + 5 ) -").Success());
+            Assert.Equal(" -", value.Match("( 1 + 1 + 1 ) - ( 10 + 5 ) -").RemainingText());
+
+            Assert.True(value.Match("+ ( 1 + 1 + 1 ) + ( ( 5 - 2 ) * 10 )").Success());
+            Assert.Equal("+ ( 1 + 1 + 1 ) + ( ( 5 - 2 ) * 10 )", value.Match("+ ( 1 + 1 + 1 ) + ( ( 5 - 2 ) * 10 )").RemainingText());
+
+            Assert.True(value.Match("( 1 + 1 + 1 ) + / 5)").Success());
+            Assert.Equal(" + / 5 )", value.Match("( 1 + 1 + 1 ) + / 5 )").RemainingText());
+
+            Assert.True(value.Match("( 1 + 1 + 1 ) * + 5").Success());
+            Assert.Equal(" * + 5", value.Match("( 1 + 1 + 1 ) * + 5").RemainingText());
+
+            Assert.True(value.Match("( 1 - 1 + - 1 ) * 5").Success());
+            Assert.Equal("( 1 - 1 + - 1 ) * 5", value.Match("( 1 - 1 + - 1 ) * 5").RemainingText());
+
+            Assert.True(value.Match("( 1 - 1 ) + / ( 100 * 200 ) + 5").Success());
+            Assert.Equal(" + / ( 100 * 200 ) + 5", value.Match("( 1 - 1 ) + / ( 100 * 200 ) + 5").RemainingText());
+
+            Assert.True(value.Match("( 5 + 10 (").Success());
+            Assert.Equal("( 5 + 10 (", value.Match("( 5 + 10 (").RemainingText());
+
+            Assert.True(value.Match("( 5 + 10 ) + ) 400 - 1000 (").Success());
+            Assert.Equal(" + ) 400 - 1000 (", value.Match("( 5 + 10 ) + ) 400 - 1000 (").RemainingText());
+        }
+
+        [Fact]
+
+        public void ReturnRemainingTextForANonEcuationString()
+        {
+            var value = new Ecuation();
+            Assert.True(value.Match("abcd").Success());
+            Assert.Equal("abcd", value.Match("abcd").RemainingText());
+
         }
     }
 }
