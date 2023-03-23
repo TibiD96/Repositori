4c2ccfa17a5d1f273faaf6783c2128a0f43e667a
TibiD96 <danitibi18@gmail.com>
2023-03-24 00:31:19 +0200

    IntArrayClass has been refractored, IntArrayTests has been refractored.

---
+++ Colection/IntArrayTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace CollectionData
+{
+    public class IntArrayTests
+    {
+        [Fact]
+
+        public void ReturnNullForNullAndEmptyForEmpty()
+        {
+            var input = new IntArray();
+            input.Add(5);
+            input.Add(10);
+            input.Add(100);
+            input.Add(25);
+            Assert.Equal(4, input.Count());
+            Assert.Equal(25, input.Element(input.Count() - 1));
+        }
+    }
+}
