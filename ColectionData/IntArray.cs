5f0e6987e626e94ff0411eb5d32e2a5a079331e0
TibiD96 <danitibi18@gmail.com>
2023-03-23 23:59:27 +0200

    IntArray Project

---
+++ ColectionData/IntArray.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ColectionData
+{
+    class IntArray
+    {
+        private int[] input;
+	
+        public IntArray()
+        {
+            this.input = new int[0];
+        }
+
+        public void Add(int element)
+        {
+            Array.Resize<int>(ref input, input.Length + 1);
+            input[input.Length - 1] = element;
+            
+        }
+
+        public int Count()
+        {
+            return input.Length;
+        }
+
+        /*public int Element(int index)
+        {
+            // întoarce elementul de la indexul dat
+        }
+
+        public void SetElement(int index, int element)
+        {
+            // modifică valoarea elementului de la indexul dat
+        }
+
+        public bool Contains(int element)
+        {
+            // întoarce true dacă elementul dat există în șir
+        }
+
+        public int IndexOf(int element)
+        {
+            // întoarce indexul elementului sau -1 dacă elementul nu se
+            // regăsește în șir
+        } */
+
+        public void Insert(int index, int element)
+        {
+            // adaugă un nou element pe poziția dată
+        }
+
+        public void Clear()
+        {
+            // șterge toate elementele din șir
+        }
+
+        public void Remove(int element)
+        {
+            // șterge prima apariție a elementului din șir	
+        }
+
+        public void RemoveAt(int index)
+        {
+            // șterge elementul de pe poziția dată
+        }
+
+    }
+
+}
---
+++ ColectionData/IntArrayTestt.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace ColectionData
+{
+    public class IntArrayTestt
+    {
+        [Fact]
+
+        public void ReturnNullForNullAndEmptyForEmpty()
+        {
+            var input = new IntArray();
+            input.Add(5);
+            Assert.Equal(1, input.Count());
+        }
+    }
+}
