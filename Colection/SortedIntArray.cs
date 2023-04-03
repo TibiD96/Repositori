8868d13735104c66b68eb3e180c8be0c240c0bc8
TibiD96 <danitibi18@gmail.com>
2023-04-03 18:44:04 +0300

    Reddundant If has been removed.

--- Colection/SortedIntArray.cs
+++ Colection/SortedIntArray.cs
@@ -36,12 +36,7 @@ namespace CollectionData
 
         private bool CheckTheElementToNotBreakTheSorting(int index, int element)
         {
-            if (index == 0 && base[index] >= element || index > 0 && base[index - 1] < element && base[index] >= element)
-            {
-                return true;
-            }
-
-            return false;
+            return index == 0 && base[index] >= element || index > 0 && base[index - 1] < element && base[index] >= element;
         }
 
         private void Sorting()
