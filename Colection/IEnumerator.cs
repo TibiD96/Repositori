00210eb3d9715c76cc2fd83f730d538141966171
TibiD96 <danitibi18@gmail.com>
2023-04-04 22:15:48 +0300

    IEnumerator class has been renamed

--- Colection/IEnumerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections;
-
-namespace CollectionData
-{
-    public class IEnumerator
-    {
-    }
-}
---
+++ Colection/ObjectEnumerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace CollectionData
+{
+    public class ObjectEnumerator : IEnumerator
+    {
+        private ObjectArray objCollection;
+        private int index = -1;
+
+        public ObjectEnumerator(ObjectArray objCollection)
+        {
+            this.objCollection = objCollection;
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
