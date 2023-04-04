5aefa0048c889722a940a8b5ee887bdd45c6a650
TibiD96 <danitibi18@gmail.com>
2023-04-04 17:41:04 +0300

    Case for Insert and Clear have been added in Object

--- Colection/ObjectArrayTests.cs
+++ Colection/ObjectArrayTests.cs
@@ -31,7 +31,7 @@ namespace CollectionData
 
         [Fact]
 
-       public void ReturnTrueIfElementIsContained()
+        public void ReturnTrueIfElementIsContained()
         {
             var obj = new ObjectArray();
             string elementToCheck = "qwerty";
@@ -41,6 +41,63 @@ namespace CollectionData
             obj.Add(123);
             obj.Add(12.3);
             Assert.True(obj.Contains(elementToCheck));
+        }
+
+        [Fact]
+
+        public void ReturnFalseIfElementIsContained()
+        {
+            var obj = new ObjectArray();
+            string elementToCheck = "zxc";
+            obj.Add("qwerty");
+            obj.Add('a');
+            obj.Add('A');
+            obj.Add(123);
+            obj.Add(12.3);
+            Assert.False(obj.Contains(elementToCheck));
+        }
+
+        [Fact]
+
+        public void ReturnTheIndexOfGivenNumberElseReturnMinusOne()
+        {
+            var obj = new ObjectArray();
+            obj.Add("qwerty");
+            obj.Add('a');
+            obj.Add('A');
+            obj.Add(123);
+            obj.Add(12.3);
+            Assert.Equal(2, obj.IndexOf('A'));
+            Assert.Equal(-1, obj.IndexOf(25.5));
+        }
+
+        [Fact]
+
+        public void InsertTheNumberToTheSpecifiedPosition()
+        {
+            var obj = new ObjectArray();
+            obj.Add("qwerty");
+            obj.Add('a');
+            obj.Add('A');
+            obj.Add(123);
+            obj.Add(12.3);
+            obj.Insert(2, 3);
+            Assert.Equal('B', obj[2]);
+            Assert.Equal('A', obj[3]);
+        }
+
+        [Fact]
+
+        public void DeleteAllElementsFfromArray()
+        {
+            var obj = new ObjectArray();
+            obj.Add("qwerty");
+            obj.Add('a');
+            obj.Add('A');
+            obj.Add(123);
+            obj.Add(12.3);
+            obj.Clear();
+            Assert.Equal(0, obj.Count);
         }
     }
 }
