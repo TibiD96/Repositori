da73e2a6f739c8cf7fef542ab482ed8f942e525c
TibiD96 <danitibi18@gmail.com>
2023-04-06 22:51:57 +0300

    Unitest for ObjectEnumerator has been added.

---
+++ Colection/ObjectEnumeratorTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace CollectionData
+{
+    public class ObjectEnumeratorTests
+    {
+        [Fact]
+
+        public void CheckIfConstructorWorks()
+        {
+            var objEnum = new ObjectArray {123, 'A', 'a', 'B', 'b', "asd"};
+            var enumerator = objEnum.GetEnumerator();
+
+            enumerator.MoveNext();
+            Assert.Equal(123, enumerator.Current);
+            enumerator.MoveNext();
+            Assert.Equal('A', enumerator.Current);
+            enumerator.MoveNext();
+            Assert.Equal('a', enumerator.Current);
+            enumerator.MoveNext();
+            Assert.Equal('B', enumerator.Current);
+            enumerator.MoveNext();
+            Assert.Equal('b', enumerator.Current);
+            enumerator.MoveNext();
+            Assert.Equal("asd", enumerator.Current);
+        }
+    }
+}
