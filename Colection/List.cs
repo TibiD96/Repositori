4bed807f4887a49ba1139a63c36b5174cdffab5a
Tiberiu Daniel Fazakas <dani.tibi18@gmail.com>
2023-04-08 19:15:58 +0300

    List class and listTests have been added.

---
+++ Colection/List.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+
+namespace CollectionData
+{
+    public class List<T> : IEnumerable
+    {
+        private object[] input;
+
+        public List()
+        {
+            this.input = new object[4];
+        }
+
+        public int Count { get; private set; }
+
+        public object this[int index]
+        {
+            get => input[index];
+            set => input[index] = value;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return input[i];
+            }
+        }
+
+        public void Add(T element)
+        {
+            Resizing();
+            input[Count++] = element;
+        }
+
+        public bool Contains(T element)
+        {
+            return IndexOf(element) > -1;
+        }
+
+        public int IndexOf(T element)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (input[i].Equals(element))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T element)
+        {
+            Resizing();
+            ShiftRight(index);
+            input[index] = element;
+            Count++;
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+        }
+
+        public void Remove(T element)
+        {
+            int indexOfElementToBeRemoved = IndexOf(element);
+            RemoveAt(indexOfElementToBeRemoved);
+        }
+
+        public void RemoveAt(int index)
+        {
+            ShiftLeft(index);
+            Count--;
+        }
+
+        private void ShiftRight(int index)
+        {
+            for (int i = Count - 1; i >= index; i--)
+            {
+                input[i + 1] = input[i];
+            }
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i <= Count - 1; i++)
+            {
+                input[i] = input[i + 1];
+            }
+        }
+
+        private void Resizing()
+        {
+            if (Count == input.Length)
+            {
+                Array.Resize(ref input, input.Length * 2);
+            }
+        }
+    }
+}
