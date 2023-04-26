1f56ca24afd9e2ac6c7ba6d1291c9ff05de07333
TibiD96 <danitibi18@gmail.com>
2023-04-26 22:30:52 +0300

    Constructor has been made adn unitest for it.

---
+++ Stream/Stream.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Stream
+{
+    public class Stream
+    {
+        public string Name { get; set; }
+        public int Length { get; set; }
+        
+        public Stream(string name, int length)
+        {
+            Name = name;
+            Length = length;
+        }
+       
+    }
+}
---
+++ Stream/StreamTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Stream
+{
+    public class StreamTests
+    {
+        [Fact]
+
+        public void CheckIfConstructorWorks()
+        {
+            Stream file = new Stream("test stream", 10);
+            Assert.Equal("test stream", file.Name);
+            Assert.Equal(10, file.Length);
+
+        }
+    }
+}
