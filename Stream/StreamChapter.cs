aac980ac3802a02b25ef36b4c6552d512cd72212
TibiD96 <danitibi18@gmail.com>
2023-05-03 22:44:25 +0300

    Gzip has been refractored. Crypting has been started.

---
+++ Stream/StreamChapter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Security.Cryptography;
+
+namespace StreamClassProgram
+{
+    public class StreamChapter
+    {
+        public MemoryStream Memory { get; private set; }
+        
+        public StreamChapter()
+        {
+            Memory = new MemoryStream();
+        }
+
+        public static void Writer(Stream stream, string text, bool gzip = false, bool crypt = false)
+        {
+            StreamWriter writer;
+            Stream support = stream;
+            if (gzip)
+            {
+               support = new GZipStream(stream, CompressionMode.Compress, true);
+            }
+
+            if (crypt)
+            {
+                using Aes aes = Aes.Create();
+                aes.GenerateKey();
+                aes.GenerateIV();
+
+                ICryptoTransform encryptor = aes.CreateEncryptor();
+
+                support = new CryptoStream(support, encryptor, CryptoStreamMode.Write, true);
+            }
+
+            writer = new StreamWriter(support, leaveOpen: true);
+            writer.Write(text);
+            writer.Flush();
+
+        }
+
+        public static string Reader(Stream stream, bool gzip = false, bool crypt = false)
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+            Stream support = stream;
+
+            if (gzip)
+            {
+                support = new GZipStream(stream, CompressionMode.Decompress, true);
+            }
+            
+            if (crypt)
+            {
+                using Aes aes = Aes.Create();
+                byte[] iv = new byte[aes.IV.Length];
+                stream.Read(iv, 0, iv.Length);
+                aes.IV = iv;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor();
+
+                support = new CryptoStream(support, decryptor, CryptoStreamMode.Read, true);
+            }
+            using var reader = new StreamReader(support);
+            return reader.ReadToEnd();
+            
+        }
+    }
+}
---
+++ Stream/StreamChapterTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+namespace StreamClassProgram
+{
+    public class StreamChapterTests
+    {
+        [Fact]
+
+        public void CheckIfConstructorWorks()
+        {
+            var file = new MemoryStream();
+            Assert.NotNull(file);
+        }
+
+        [Fact]
+
+        public void CheckIfWriterAndReaderMethodeWorks()
+        {
+            var stream = new MemoryStream();
+            StreamChapter.Writer(stream, "test");
+            string textToCheck = StreamChapter.Reader(stream);
+            Assert.Equal("test", textToCheck);
+        }
+
+        [Fact]
+
+        public void CheckIfCompressAndDecompressMethodesWorks()
+        {
+            var stream = new MemoryStream();
+            StreamChapter.Writer(stream, "test", true);
+            string result = StreamChapter.Reader(stream, true);
+            Assert.Equal("test", result);
+        }
+
+        [Fact]
+
+        public void CheckIfCryptAndDecryptMethodesWorks()
+        {
+            var stream = new MemoryStream();
+            StreamChapter.Writer(stream, "test", false, true);
+            string result = StreamChapter.Reader(stream, false, true);
+            Assert.Equal("test", result);
+        }
+    }
+}
