868685b5adee5fcefedc93539d907224deca5e0f
TibiD96 <danitibi18@gmail.com>
2023-05-13 20:40:40 +0300

    StreamChapter has been modified to don't contain any stream construction inside writer and reader. StreamBuilder has been generated to create streams for different condisions like encrypt decrypt compress and decompress.

--- Stream/StreamChapter.cs
+++ Stream/StreamChapter.cs
@@ -5,54 +5,44 @@ using System.Security.Cryptography;
 
 namespace StreamClassProgram
 {
-    public class StreamChapter
+    public class StreamChapter : StreamBuilder
     {
-        public static Aes aes { get; } = Aes.Create();
-
         public static void Writer(Stream stream, string text, bool gzip = false, bool crypt = false)
         {
-            Stream support = stream;
-            StreamWriter writer;
-
             if (gzip)
             {
-                support = new GZipStream(support, CompressionMode.Compress);
+                GzipedStream(stream);
             }
 
             if (crypt)
             {
-                ICryptoTransform encryptor = aes.CreateEncryptor();
-                support = new CryptoStream(support, encryptor, CryptoStreamMode.Write);
+                CryptStream(stream);
             }
 
-            writer = new StreamWriter(support);
+            StreamWriter writer = new StreamWriter(stream);
             writer.Write(text);
             writer.Flush();
-
-            if (support is CryptoStream cryptoStream)
+            if (stream is CryptoStream cryptoStream)
             {
-                cryptoStream.FlushFinalBlock();
-            }
+              cryptoStream.FlushFinalBlock();
+            }         
         }
 
         public static string Reader(Stream stream, bool gzip = false, bool crypt = false)
         {
-            stream.Seek(0, SeekOrigin.Begin);
-            Stream support = stream;
-
             if (gzip)
             {
-                support = new GZipStream(support, CompressionMode.Decompress);
+                UnzipedStream(stream);
             }
 
             if (crypt)
             {
-                ICryptoTransform decryptor = aes.CreateDecryptor();
-                support = new CryptoStream(support, decryptor, CryptoStreamMode.Read);
+                DecryptStream(stream);
             }
-
-            using StreamReader reader = new StreamReader(support);
+            using StreamReader reader = new StreamReader(stream); 
             return reader.ReadToEnd();
+                
+            
         }
     }
 }
