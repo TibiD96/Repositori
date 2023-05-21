37684a09f1b34e42e36a448a34baee4559b616cc
TibiD96 <danitibi18@gmail.com>
2023-05-21 21:03:42 +0300

    DoubleCahinedList class and LinkedListNode class have been added.

---
+++ ListaDubluInlantuita/DoubleChainedList.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Security.Cryptography;
+
+
+namespace ChainedList
+{
+    public class DoubleChainedList<T> : ICollection<T>
+    {
+        private readonly LinkedListNode<T> sentinel = new LinkedListNode<T>(default);
+        public DoubleChainedList()
+        {
+            sentinel.Next = sentinel;
+            sentinel.Previous = sentinel;
+        }
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly { get; private set; }
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ListaDubluInlantuita/LinkedListNode.cs
@@ -0,0 +1,20 @@
+﻿using ChainedList;
+
+namespace ChainedList
+{
+    public class LinkedListNode<T>
+    {
+        public LinkedListNode(T item)
+        {
+            Value = item;
+        }
+
+        public T Value { get; set; }
+
+        public LinkedListNode<T> Next { get; set; }
+
+        public LinkedListNode<T> Previous { get; set; }
+
+        public DoubleChainedList<T> List { get; internal set; }
+    }
+}
