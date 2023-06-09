ae2926c2f75b9e06ede9bf7627e59ee9fb5b8033
TibiD96 <danitibi18@gmail.com>
2023-06-09 22:10:28 +0300

    Methodes of Dictionary have been implemented.

--- DictionaryCollection/DictionaryList.cs
+++ DictionaryCollection/DictionaryList.cs
@@ -1,6 +1,75 @@
 ﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DictionaryCollection
 {
+    public class Dictionary<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+        private readonly Dictionary<int, string> alfabet = new Dictionary<int, string>();
 
+        public ICollection<TKey> Keys => throw new NotImplementedException();
+
+        public ICollection<TValue> Values => throw new NotImplementedException();
+
+        public int Count { get; }
+
+        public bool IsReadOnly { get; set; }
+
+        public TValue this[TKey key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Add(TKey key, TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
