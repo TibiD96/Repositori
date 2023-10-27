22dbb349c36a3da3549f7585a17037bebd0e6a7c
TibiD96 <danitibi18@gmail.com>
2023-10-27 23:15:41 +0300

    All, Any and First methodes have been adde and unitest have been made for them and passed.

---
+++ Linq/LinqMethodes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Linq
+{
+    public static class LinqMethodes
+    {
+        public static bool All<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null || predicate == null)
+            {
+                throw new ArgumentNullException("Can't be null");
+            }
+
+            foreach (var element in source)
+            {
+                if (!predicate(element))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool Any<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null || predicate == null)
+            {
+                throw new ArgumentNullException("Can't be null");
+            }
+
+            foreach (var element in source)
+            {
+                if (predicate(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static TSource First<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null || predicate == null)
+            {
+                throw new ArgumentNullException("Can't be null");
+            }
+
+            foreach (var element in source)
+            {
+                if (predicate(element))
+                {
+                    return element;
+                }
+            }
+
+            throw new InvalidOperationException("No element respect condition");
+        }
+    }
+}
---
+++ Linq/LinqMethodesTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using Linq;
+using Xunit;
+
+namespace Linq
+{
+    public class LinqMethodesTests
+    {
+        [Fact]
+
+        public void CheckAllMethode()
+        {
+            var first = new int[4];
+            first[0] = 1;
+            first[1] = 3;
+            first[2] = 5;
+            first[3] = 7;
+
+            var second = new int[4];
+            second[0] = 2;
+            second[1] = 4;
+            second[2] = 6;
+            second[3] = 8;
+
+            Assert.True(first.All(x => x % 2 == 1));
+            Assert.False(second.All(x => x % 2 == 1));
+            Assert.Throws<ArgumentNullException>(() => LinqMethodes.All<int>(null, x => x % 2 == 1));
+        }
+
+        [Fact]
+
+        public void CheckAnyMethode()
+        {
+            var first = new int[4];
+            first[0] = 2;
+            first[1] = 3;
+            first[2] = 4;
+            first[3] = 8;
+
+            var second = new int[4];
+            second[0] = 2;
+            second[1] = 4;
+            second[2] = 6;
+            second[3] = 8;
+
+            Assert.True(first.Any(x => x % 2 == 1));
+            Assert.False(second.Any(x => x % 2 == 1));
+            Assert.Throws<ArgumentNullException>(() => LinqMethodes.Any<int>(null, x => x % 2 == 1));
+        }
+
+        [Fact]
+
+        public void CheckFirstMethode()
+        {
+            var first = new int[4];
+            first[0] = 2;
+            first[1] = 3;
+            first[2] = 4;
+            first[3] = 8;
+
+            var second = new int[4];
+            second[0] = 2;
+            second[1] = 4;
+            second[2] = 6;
+            second[3] = 8;
+
+            Assert.Equal(first[1], first.First(x => x % 2 == 1));
+            Assert.Throws<InvalidOperationException>(() => second.First(x => x % 2 == 1));
+            Assert.Throws<ArgumentNullException>(() => LinqMethodes.First<int>(null, x => x % 2 == 1));
+        }
+    }
+}
