265afbf3d8c2b8f21fbb17b3f524105ac33365e9
TibiD96 <danitibi18@gmail.com>
2023-12-13 22:43:11 +0200

    Classes for next set of problems with integers added

---
+++ Linq/IntArrayFunctions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    public class IntArrayFunctions
+    {
+        public List<List<int>> SumLowerThenK(int k, int[] intArray)
+        {
+
+        }
+    }
+}
---
+++ Linq/IntArrayFunctionsTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Linq
+{
+    public class IntArrayFunctionsTests
+    {
+        [Fact]
+
+        public void SumLowerThenK()
+        {
+
+        }
+    }
+}
