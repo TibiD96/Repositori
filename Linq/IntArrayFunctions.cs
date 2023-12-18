141607ef96c7ce05b7cc5df78dedd83fd8c6df67
TibiD96 <danitibi18@gmail.com>
2023-12-19 00:17:42 +0200

    SumLowerEqualThenK has been added and unitest has been passed.

--- Linq/IntArrayFunctions.cs
+++ Linq/IntArrayFunctions.cs
@@ -6,9 +6,33 @@ namespace Linq
 {
     public class IntArrayFunctions
     {
-       // public List<List<int>> SumLowerThenK(int k, int[] intArray)
-        //{
+       public static List<List<int>> SumLowerEqualThenK(int[] intArray, int k)
+        {
+            List<List<int>> list = new List<List<int>>();
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                List<int> subList = new List<int>();
+                int currentSum = 0;
 
-        //}
+                for (int j = i + 1; j <= intArray.Length; j++)
+                {
+                    foreach (int c in intArray[i..j])
+                    {
+                        currentSum += c;
+                    }
+
+                    if (currentSum <= k)
+                    {
+                        subList.AddRange(intArray[i..j]);
+                        list.Add(subList);
+                        currentSum = 0;
+                    }
+
+                    subList = new List<int>();
+                }
+            }
+
+            return list;
+        }
     }
 }
--- Linq/IntArrayFunctionsTests.cs
+++ Linq/IntArrayFunctionsTests.cs
@@ -9,9 +9,29 @@ namespace Linq
     {
         [Fact]
 
-        public void SumLowerThenK()
+        public void SumLowerEqualThenK()
         {
+            int[] arrayInt = { 1, 2, 3, 4, 6 };
+            const int number = 10;
+            var expected = new List<List<int>>()
+            {
+                new List<int> { 1 },
+                new List<int> { 1, 2 },
+                new List<int> { 1, 2, 3 },
+                new List<int> { 1, 2, 3, 4 },
+                new List<int> { 2 },
+                new List<int> { 2, 3 },
+                new List<int> { 2, 3, 4 },
+                new List<int> { 3 },
+                new List<int> { 3, 4 },
+                new List<int> { 4 },
+                new List<int> { 4, 6 },
+                new List<int> { 6 }
+            };
 
+            var result = IntArrayFunctions.SumLowerEqualThenK(arrayInt, number);
+
+            Assert.Equal(expected, result);
         }
     }
 }
