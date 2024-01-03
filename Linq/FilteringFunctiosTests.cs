34489db2512ac2dc00252692a2e049289ee24bc6
TibiD96 <danitibi18@gmail.com>
2024-01-04 00:27:20 +0200

    Unitest for NoFeature method has been added.

--- Linq/FilteringFunctiosTests.cs
+++ Linq/FilteringFunctiosTests.cs
@@ -66,5 +66,34 @@ namespace Linq
 
             Assert.Equal(expected, final);
         }
+
+        [Fact]
+
+        public void NoFeature()
+        {
+            var inputListProd = new List<ProductWithFeature>()
+            {
+                new ProductWithFeature("RcCar", 1, 2, 3, 4, 5),
+                new ProductWithFeature("Phone", 3, 5),
+                new ProductWithFeature("Tv", 10, 25),
+                new ProductWithFeature("Ball", 2)
+            };
+
+            var inputFeatures = new List<Feature>()
+            {
+                new Feature(1),
+                new Feature(5),
+                new Feature(2)
+            };
+
+            var expected = new List<ProductWithFeature>()
+            {
+                new ProductWithFeature("Tv", 10, 25)
+            };
+
+            var final = FilteringFunctions.NoFeature(inputListProd, inputFeatures);
+
+            Assert.Equal(expected, final);
+        }
     }
 }
