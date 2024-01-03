426540be96bbfe374506ecc1c5a24ef4aefece1a
TibiD96 <danitibi18@gmail.com>
2024-01-03 22:36:37 +0200

    Unitest for AllFeature methode has been added.

--- Linq/FilteringFunctiosTests.cs
+++ Linq/FilteringFunctiosTests.cs
@@ -37,5 +37,34 @@ namespace Linq
 
             Assert.Equal(expected, final);
         }
+
+        [Fact]
+
+        public void ALLFeature()
+        {
+            var inputListProd = new List<ProductWithFeature>()
+            {
+                new ProductWithFeature("RcCar", 1, 2, 3, 4, 5),
+                new ProductWithFeature("Phone", 3, 5),
+                new ProductWithFeature("Tv", 10, 25),
+                new ProductWithFeature("Ball", 2)
+            };
+
+            var inputFeatures = new List<Feature>()
+            {
+                new Feature(1),
+                new Feature(5),
+                new Feature(2)
+            };
+
+            var expected = new List<ProductWithFeature>()
+            {
+                new ProductWithFeature("RcCar", 1, 2, 3, 4, 5)
+            };
+
+            var final = FilteringFunctions.AtLeastOneFeature(inputListProd, inputFeatures);
+
+            Assert.Equal(expected, final);
+        }
     }
 }
