3e8fcc366126f1aab045e5607d6353e8685b985b
TibiD96 <danitibi18@gmail.com>
2024-01-04 16:43:26 +0200

    TestResults class has been added.

---
+++ Linq/TestResults.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    public class TestResults
+    {
+        public string Id { get; set; }
+
+        public string FamilyId { get; set; }
+
+        public int Score { get; set; }
+
+        public TestResults(string id, string familyId, int score)
+        {
+            Id = id;
+            FamilyId = familyId;
+            Score = score;
+        }
+    }
+}
