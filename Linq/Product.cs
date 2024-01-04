0929e99033453243ba51cf8dcb10610f4b11e7e2
TibiD96 <danitibi18@gmail.com>
2024-01-04 13:24:28 +0200

    Product class has been added.

---
+++ Linq/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public class Product
+    {
+        public string Name;
+        public int Quantity;
+
+        public Product(string name, int quantity)
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+    }
+}
