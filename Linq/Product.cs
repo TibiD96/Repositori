e648424945a18eb6169e4c862b298c8f5890b116
TibiD96 <danitibi18@gmail.com>
2024-01-04 13:25:34 +0200

    changed from class to struct

--- Linq/Product.cs
+++ Linq/Product.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Linq
 {
-    public class Product
+    public struct Product
     {
         public string Name;
         public int Quantity;
