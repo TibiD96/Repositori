9056dfbadf292f785ce95b1439dfccea92f1ce1b
TibiD96 <danitibi18@gmail.com>
2024-01-05 18:36:50 +0200

    Unitest for Invalid sudoku has been added and passed.

--- Linq/FilteringFunctiosTests.cs
+++ Linq/FilteringFunctiosTests.cs
@@ -230,7 +230,7 @@ namespace Linq
 
         [Fact]
 
-        public void CheckSudoku()
+        public void CheckSudokuToBeValid()
         {
             int[,] inputSudokuTable = new int[,]
             {
@@ -246,6 +246,26 @@ namespace Linq
             };
 
             Assert.True(FilteringFunctions.SudokuValidator(inputSudokuTable));
+        }
+
+        [Fact]
+
+        public void CheckSudokuToBeInvalid()
+        {
+            int[,] inputSudokuTable = new int[,]
+            {
+                { 5, 3, 4, 1, 7, 8, 9, 1, 2 },
+                { 6, 7, 2, 1, 9, 5, 3, 4, 8 },
+                { 1, 9, 8, 3, 4, 2, 5, 6, 7 },
+                { 8, 5, 9, 7, 6, 1, 4, 2, 3 },
+                { 4, 2, 6, 8, 5, 3, 7, 9, 1 },
+                { 7, 1, 3, 9, 2, 4, 8, 5, 6 },
+                { 9, 6, 1, 5, 3, 7, 2, 8, 4 },
+                { 2, 8, 7, 4, 1, 9, 6, 3, 5 },
+                { 3, 4, 5, 2, 8, 6, 1, 7, 9 }
+            };
+
+            Assert.False(FilteringFunctions.SudokuValidator(inputSudokuTable));
         }
     }
 }
