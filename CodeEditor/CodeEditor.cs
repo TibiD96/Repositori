57c36515d9c90296e5942c1b8d5927ed2782dd05
Tiberiu Daniel Fazakas <dani.tibi18@gmail.com>
2024-02-08 00:08:40 +0200

    Infracstructure for CodeEditor App has been added.

---
+++ CodeEditor/CodeEditor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEditor
+{
+    public class CodeEditor
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ CodeEditor/Consola.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEditor
+{
+    public class Consola
+    {
+        public static void Menu()
+        {
+            Console.WriteLine("Show from file");
+        }
+    }
+}
