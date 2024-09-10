51cc33faa218473782b6c26b464676dc90612258
TibiD96 <danitibi18@gmail.com>
2024-09-10 23:43:09 +0300

    New .dll files has been created old .dll and .so files have been deleted

---
+++ CodeEditor/NativeMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CodeEditor
+{
+    internal static class NativeMethods
+    {
+        [DllImport("tree-sitter", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr ts_parser_new();
+    }
+}
